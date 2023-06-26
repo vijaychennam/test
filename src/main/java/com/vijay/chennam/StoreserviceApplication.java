a3a3ea674f47ddde40554f58e7c082e7152ab46e
Vijay Chennam <vijaychennam@gmail.com>
2023-06-26 22:13:55 +0530

    Initial commit

---
+++ src/main/java/com/vijay/chennam/StoreserviceApplication.java
@@ -0,0 +1,13 @@
+package com.vijay.chennam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vijay/chennam/StoreserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vijay.chennam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
